b22aff2eb50747d4f40d791cfcc39c7ae5f04c5e
Lucie OUVRIER-BUFFET <lucie.ouvrier-buffet@grenoble-inp.org>
2018-04-03 11:30:16 +0200

    method to add a tag filled

--- database/Database.py
+++ database/Database.py
@@ -1,29 +1,75 @@
 import os
-from model.DatabaseModel import createDatabase, Base
-from sqlalchemy import create_engine
+from model.DatabaseModel import createDatabase, Base, Tag, Path_Initial, Path_Current, TAG_TYPE_STRING, TAG_TYPE_INTEGER, TAG_TYPE_FLOAT
+from sqlalchemy import create_engine, Column, String, Integer, Float
 from sqlalchemy.orm import sessionmaker
 
 class Database:
 
     def __init__(self, path):
+        """
+        Creates the database instance that will do the API between the database and the software
+        :param path: Path of the database file
+        """
+
         self.path = path
 
         # We create the database file if it does not already exist
         if not os.path.exists(self.path):
             createDatabase(self.path)
 
-        engine = create_engine('sqlite:///' + self.path)
-        Base.metadata.bind = engine
-        DBSession = sessionmaker(bind=engine)
+        self.engine = create_engine('sqlite:///' + self.path)
+        Base.metadata.bind = self.engine
+        DBSession = sessionmaker(bind=self.engine)
         self.session = DBSession()
 
     """ TAGS """
 
-    def add_tag(self, tag, visible, origin, type, unit, default_value, description):
+    def add_tag(self, name, visible, origin, type, unit, default_value, description):
+        """
+        Adds a tag to the database if it does not already exist
+        :param name: Tag name
+        :param visible: Tag visibility (True or False)
+        :param origin: Tag origin (Raw or user)
+        :param type: Tag type (string, integer, float, date, datetime, time, list_string, list_integer, list_float, list_date, list_datetime, or list_time)
+        :param unit: Tag unit (ms, mm, degree, Hz/pixel, or MHz)
+        :param default_value: Tag default value
+        :param description: Tag description
+        """
+
+        # We add the tag if it does not already exist
+        if self.get_tag(name) is None:
+
+            # Adding the tag in the Tag table
+            tag = Tag(name=name, visible=visible, origin=origin, type=type, unit=unit, default_value=default_value, description=description)
+            self.session.add(tag)
+
+            # Adding the tag in the Path tables
+            if type == TAG_TYPE_INTEGER:
+                column = Column(name, Integer, nullable=True)
+            elif type == TAG_TYPE_FLOAT:
+                column = Column(name, Float, nullable=True)
+            else:
+                column = Column(name, String, nullable=True)
+            column_name = column.compile(dialect=self.engine.dialect)
+            column_type = column.type.compile(self.engine.dialect)
+            self.engine.execute('ALTER TABLE %s ADD COLUMN %s %s' % ("path_initial", column_name, column_type))
+            self.engine.execute('ALTER TABLE %s ADD COLUMN %s %s' % ("path_current", column_name, column_type))
+
+    def remove_tag(self, name):
         pass
 
-    def remove_tag(self, tag):
-        pass
+    def get_tag(self, name):
+        """
+        Gives the Tag object of the tag
+        :param name: Tag name
+        :return: The tag object if the tag exists, None otherwise
+        """
+
+        tags = self.session.query(Tag).filter(Tag.name == name).all()
+        if len(tags) == 1:
+            return tags[0]
+        else:
+            return None
 
     """ VALUES """
 
