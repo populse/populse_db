df7c81448deec6eb06041fe1c6d2b7ff7caa7588
Lucie OUVRIER-BUFFET <lucie.ouvrier-buffet@grenoble-inp.org>
2018-04-04 15:44:25 +0200

    removing values when removing scan + comments

--- Test.py
+++ Test.py
@@ -48,6 +48,8 @@ class TestDatabaseMethods(unittest.TestCase):
         database = Database(path)
         database.add_tag("PatientName", True, TAG_ORIGIN_RAW, TAG_TYPE_STRING, None, None, "Name of the patient")
         tag = database.get_tag("PatientName")
+
+        # Checking the tag properties
         self.assertEqual(tag.name, "PatientName")
         self.assertTrue(tag.visible)
         self.assertEqual(tag.origin, TAG_ORIGIN_RAW)
@@ -55,6 +57,7 @@ class TestDatabaseMethods(unittest.TestCase):
         self.assertIsNone(tag.unit)
         self.assertIsNone(tag.default_value)
         self.assertEqual(tag.description, "Name of the patient")
+
         # TODO Testing tag table creation
 
     def test_get_tag(self):
@@ -67,8 +70,12 @@ class TestDatabaseMethods(unittest.TestCase):
             os.remove(path)
         database = Database(path)
         database.add_tag("PatientName", True, TAG_ORIGIN_RAW, TAG_TYPE_STRING, None, None, "Name of the patient")
+
+        # Testing that the tag is returned if it exists
         tag = database.get_tag("PatientName")
         self.assertIsInstance(tag, database.classes["tag"])
+
+        # Testing that None is returned if the tag does not exist
         tag = database.get_tag("Test")
         self.assertIsNone(tag)
 
@@ -85,6 +92,7 @@ class TestDatabaseMethods(unittest.TestCase):
         database.remove_tag("PatientName")
         tag = database.get_tag("PatientName")
         self.assertIsNone(tag)
+
         # TODO Testing tag table removal
 
     def test_add_scan(self):
@@ -113,9 +121,14 @@ class TestDatabaseMethods(unittest.TestCase):
         database.add_scan("scan1", "159abc")
         database.add_scan("scan2", "def753")
         database.add_tag("PatientName", True, TAG_ORIGIN_RAW, TAG_TYPE_STRING, None, None, "Name of the patient")
+
+        # Testing that None is returned if the value does not exist
         value = database.get_current_value("scan1", "PatientName")
         self.assertIsNone(value)
+
         database.add_value("scan1", "PatientName", "test")
+
+        # Testing that the value is returned if it exists
         value = database.get_current_value("scan1", "PatientName")
         self.assertEqual(value, "test")
 
@@ -131,9 +144,14 @@ class TestDatabaseMethods(unittest.TestCase):
         database.add_scan("scan1", "159abc")
         database.add_scan("scan2", "def753")
         database.add_tag("PatientName", True, TAG_ORIGIN_RAW, TAG_TYPE_STRING, None, None, "Name of the patient")
+
+        # Testing that None is returned if the value does not exist
         value = database.get_initial_value("scan1", "PatientName")
         self.assertIsNone(value)
+
         database.add_value("scan1", "PatientName", "test")
+
+        # Testing that the value is returned if it exists
         value = database.get_initial_value("scan1", "PatientName")
         self.assertEqual(value, "test")
 
@@ -206,9 +224,18 @@ class TestDatabaseMethods(unittest.TestCase):
         database = Database(path)
         database.add_scan("scan1", "159abc")
         database.add_scan("scan2", "def753")
+        database.add_tag("PatientName", True, TAG_ORIGIN_RAW, TAG_TYPE_STRING, None, None, "Name of the patient")
+        database.add_value("scan1", "PatientName", "test")
+        value = database.get_current_value("scan1", "PatientName")
         database.remove_scan("scan1")
+
+        # Testing that the scan is removed from Path table
         scan = database.get_scan("scan1")
         self.assertIsNone(scan)
+
+        # Testing that the values associated are removed
+        value = database.get_current_value("scan1", "PatientName")
+        self.assertIsNone(value)
 
     def test_get_scan(self):
         """
@@ -221,8 +248,12 @@ class TestDatabaseMethods(unittest.TestCase):
         database = Database(path)
         database.add_scan("scan1", "159abc")
         database.add_scan("scan2", "def753")
+
+        # Testing that a scan is returned if it exists
         scan = database.get_scan("scan1")
         self.assertIsInstance(scan, database.classes["path"])
+
+        # Testing that None is returned if the scan does not exist
         scan = database.get_scan("scan3")
         self.assertIsNone(scan)
 
