3173702766577c765913e6cf5b1fa5994d3ce401
Lucie OUVRIER-BUFFET <lucie.ouvrier-buffet@grenoble-inp.org>
2018-07-23 11:49:23 +0200

    removing traces

--- python/populse_db/database.py
+++ python/populse_db/database.py
@@ -412,19 +412,13 @@ class DatabaseSession:
         Redefines the model after an update of the schema
         """
 
-        print("begin update_table")
         self.table_classes = {}
-        print("trace 1")
         self.base = automap_base(metadata=self.metadata)
-        print("trace 2")
-        self.base.prepare(engine=self.database.engine)
-        print("trace 3")
+        self.base.prepare(engine=self.database.engine) # Blocking here in MIA tests with Python3.7 OSX (test_add_tag)
         for table in self.metadata.tables.keys():
             print(table)
             self.table_classes[table] = getattr(
                 self.base.classes, table)
-        print("end update_table")
-
 
     """ CACHES """
 
