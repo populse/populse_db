bebf6ae3c4572fdfd9b061dbf11f3b3b3201a4c7
Lucie OUVRIER-BUFFET <lucie.ouvrier-buffet@grenoble-inp.org>
2018-08-08 17:53:35 +0200

    removing print from tests

--- python/populse_db/info.py
+++ python/populse_db/info.py
@@ -9,9 +9,9 @@
 import sys
 
 # populse_db current version
-version_major = 0
+version_major = 1
 version_minor = 0
-version_micro = 1
+version_micro = 0
 version_extra = ""
 
 # Expected by setup.py: string of form "X.Y.Z"
