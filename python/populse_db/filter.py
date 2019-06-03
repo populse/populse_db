fcf347c4c540b66a3721ea7aa4fbc2c3bf82f08a
econdami <econdami@yahoo.fr>
2019-06-03 11:22:31 +0200

    Use of the larl parser in place of the default Earley. This fixes #33

--- python/populse_db/filter.py
+++ python/populse_db/filter.py
@@ -22,7 +22,15 @@ from sqlalchemy.sql.elements import BinaryExpression
 import populse_db
 from populse_db.database import DatabaseSession
 
-# The grammar (in Lark format) used to parse filter strings
+# The grammar (in Lark format) used to parse filter strings:
+
+# Grammar with weights on terminals (raising a ValueError exception with
+# lark_parser V>= 0.7.0 and Earley parser). Because the running time is very
+# long with Earley parser, we decide to keep this grammar and to use larl
+# parser (filter_parser() and literal_parser() below). Firts tests seems
+# to indicate that this grammar give good result with this parser, and it is
+# much faster.
+
 filter_grammar = '''
 ?start : filter
 
@@ -95,6 +103,84 @@ list : "[" [literal ("," literal)*] "]"
 %ignore WS
 '''
 
+
+# Grammar without weights on terminals (for lark_parser V>= 0.7.0 and Earley
+# parser). With this grammar no ValueError exception is raised, but the runing
+# times is too long with Earley parser.
+"""
+filter_grammar = '''
+?start : filter
+
+?filter : "ALL"i                         -> all
+        | conditions
+        | negation
+        | "(" filter ")"
+        | filter BOOLEAN_OPERATOR filter -> conditions
+
+?conditions : condition (BOOLEAN_OPERATOR condition)*
+
+                   
+negation : "NOT"i condition
+         | "NOT"i "(" filter ")"
+
+BOOLEAN_OPERATOR : "AND"i
+                 | "OR"i
+
+CONDITION_OPERATOR : "=="i
+                   | "!="i
+                   | "<="i
+                   | ">="i
+                   | ">"i
+                   | "<"i
+                   | "IN"i
+                   | "ILIKE"i
+                   | "LIKE"i
+
+condition : operand CONDITION_OPERATOR operand
+
+?operand : literal
+         | field_name
+
+field_name.1 : FIELD_NAME
+             | quoted_field_name
+
+quoted_field_name.1 : QUOTED_FIELD_NAME
+
+
+         
+?literal.2 : KEYWORD_LITERAL -> keyword_literal
+         | ESCAPED_STRING   -> string
+         | SIGNED_NUMBER    -> number
+         | DATE             -> date
+         | TIME             -> time
+         | DATETIME         -> datetime
+         | list
+
+
+DATE : INT "-" INT "-" INT
+TIME : INT ":" INT (":" INT ("." INT)?)?
+DATETIME : DATE "T" TIME
+
+KEYWORD_LITERAL : "NULL"i
+                | "TRUE"i
+                | "FALSE"i
+                  
+FIELD_NAME : ("_"|LETTER) ("_"|LETTER|DIGIT)*
+QUOTED_FIELD_NAME : "{" /[^}]/* "}"
+
+list : "[" [literal ("," literal)*] "]"
+
+%import common.INT
+%import common.ESCAPED_STRING
+%import common.SIGNED_NUMBER
+%import common.LETTER
+%import common.DIGIT
+
+%import common.WS
+%ignore WS
+'''
+"""
+
 # The instance of the grammar parser is created only once
 # then stored in _grammar_parser for later reuse
 _grammar_parser = None
@@ -106,7 +192,7 @@ def filter_parser():
     '''
     global _grammar_parser
     if _grammar_parser is None:
-        _grammar_parser = Lark(filter_grammar)
+        _grammar_parser = Lark(filter_grammar, parser='lalr')
     return _grammar_parser
 
 
@@ -117,7 +203,7 @@ def literal_parser():
     :return: An instance of Lark grammar parser for parsing only a literal value (int, string, list, date, etc.) from a filter expression.
 
     '''
-    return Lark(filter_grammar, start='literal')
+    return Lark(filter_grammar, parser='lalr', start='literal')
 
 
 class FilterImplementationLimit(NotImplementedError):
