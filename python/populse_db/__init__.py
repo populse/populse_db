a3029bb448fc1943f7dbf9f703bfff9bed81eae4
Yann Leprince <yann.leprince@cea.fr>
2020-04-08 11:50:07 +0200

    add a comment mentioning a known bug

--- python/populse_db/__init__.py
+++ python/populse_db/__init__.py
@@ -1,2 +1,7 @@
+# KNOWN BUG under Python 3: When the following line is executed,
+# populse_db.database is added to sys.modules. As a result, future calls to
+# 'from populse_db.database import ...' will succeed, even if the populse_db
+# package cannot be imported (e.g. if importing populse_db.filter fails due to
+# lark-parser missing).
 from . import database
 from . import filter
