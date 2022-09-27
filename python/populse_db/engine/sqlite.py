c9d38d431b4047985b286431e60d6b5dfa14843d
sapetnioc <sapetnioc@users.noreply.github.com>
2022-09-27 13:53:16 +0200

    Compatibility with older SQlite version (especially the one used with AppVeyor)

--- python/populse_db/engine/sqlite.py
+++ python/populse_db/engine/sqlite.py
@@ -102,7 +102,7 @@ class SQLiteSession(DatabaseSession):
         Erase the whole database content.
         """
 
-        sql = f'SELECT name FROM sqlite_schema'
+        sql = f'SELECT name FROM sqlite_master'
         tables = [i[0] for i in self.execute(sql)]
         for table in tables:
             sql = f'DROP TABLE {table}'
@@ -129,7 +129,7 @@ class SQLiteSession(DatabaseSession):
         self._collection_cache.pop(name, None)
     
     def __iter__(self):
-        sql = f"SELECT name FROM sqlite_schema WHERE type='table'"
+        sql = f"SELECT name FROM sqlite_master WHERE type='table'"
         for row in self.execute(sql):
             table = row[0]
             if table == self.populse_db_table:
