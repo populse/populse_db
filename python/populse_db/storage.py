32a49d0c8ad9f5be8e491d722c81acf257efd569
sapetnioc <sapetnioc@users.noreply.github.com>
2023-12-17 11:37:47 +0100

    Make Storage schema modular

--- python/populse_db/storage.py
+++ python/populse_db/storage.py
@@ -1,18 +1,176 @@
 from contextlib import contextmanager
+import importlib
 
 from .database import type_to_str
-from .storage_server import StorageServer
+from .storage_server import StorageClient
 
 
 class Storage:
+    default_collection = "_"
+    schema_collection = "_schema"
+    default_field = "_"
+    default_document_id = "_"
+
     def __init__(self, *args, **kwargs):
-        self.server = StorageServer(*args, **kwargs)
+        self.server = StorageClient(*args, **kwargs)
+
+    @classmethod
+    def find_schema(cls, name, version_selection=None):
+        module = importlib.import_module(name)
+
+        schemas_to_collections = getattr(module, "_schemas_to_collections", None)
+        if schemas_to_collections is None:
+            schemas_to_collections = {}
+            index = 0
+            for schema_declaration in module.schemas:
+                unknown_items = set(schema_declaration) - {"version", "schema"}
+                if unknown_items:
+                    raise ValueError(
+                        f"invalid item(s) in schema {module.__name__} (list index={index}): {','.join(unknown_items)}"
+                    )
+                version = schema_declaration.get("version")
+                if version is None:
+                    raise ValueError(
+                        f"version missing in schema {module.__name__} (list index={index})"
+                    )
+                sversion = version.split(".")
+                if len(sversion) != 3:
+                    raise ValueError(
+                        f"invalid version in schema {module.__name__} (list index={index}): {version}"
+                    )
+                schema_content = schema_declaration.get("schema")
+                if schema_content is None:
+                    raise ValueError(
+                        f"schema missing in schema {module.__name__} (list index={index})"
+                    )
+                try:
+                    collections = cls._parse_schema_content(schema_content)
+                except Exception as e:
+                    raise ValueError(
+                        f"invalid schema definition {module.__name__} (list index={index})"
+                    ) from e
+                schema_to_collections = {
+                    "name": module.__name__,
+                    "version": version,
+                    "collections": collections,
+                }
+                if version in schemas_to_collections:
+                    raise ValueError(
+                        f"two schemas with version {version} defined in {module.__name__}"
+                    )
+                schemas_to_collections[version] = schema_to_collections
+                if None not in schemas_to_collections:
+                    schemas_to_collections[None] = schema_to_collections
+                short_version = ".".join(sversion[:2])
+                if short_version not in schemas_to_collections:
+                    schemas_to_collections[short_version] = schema_to_collections
+                index += 1
+            module._schemas_to_collections = schemas_to_collections
+        return schemas_to_collections.get(version_selection)
+
+    @classmethod
+    def _parse_field(cls, name, definition):
+        error = True
+        if isinstance(definition, type):
+            type_str = type_to_str(definition)
+            kwargs = {}
+            error = False
+        elif isinstance(definition, str):
+            type_str = definition
+            kwargs = {}
+            error = False
+        elif isinstance(definition, list) and len(definition) == 2:
+            if isinstance(definition[1], dict):
+                if isinstance(definition[0], type):
+                    type_str = type_to_str(definition[0])
+                    kwargs = definition[1]
+                    error = False
+                elif isinstance(definition[0], str):
+                    type_str = definition[0]
+                    kwargs = definition[1]
+                    error = False
+        if error:
+            raise ValueError(f'invalid definition for field "{name}"')
+        return [type_str, kwargs]
+
+    @classmethod
+    def _parse_schema_content(cls, schema_content):
+        if not isinstance(schema_content, dict):
+            raise ValueError("schema must be a dict")
+        collections = {}
+        for k, v in schema_content.items():
+            if isinstance(v, dict):
+                fields = {kk: cls._parse_field(kk, vv) for kk, vv in v.items()}
+                for name, d in fields.items():
+                    _, kwargs = d
+                    if name == Storage.default_field:
+                        raise ValueError(f'invalid field name: "{name}')
+                    if "primary_key" in kwargs:
+                        raise ValueError(
+                            f'primary key not allowed for unique document "{k}" in field "{name}"'
+                        )
+                fields[Storage.default_field] = ["str", {"primary_key": True}]
+                collections[k] = fields
+            elif isinstance(v, list) and len(v) == 1 and isinstance(v[0], dict):
+                fields = {kk: cls._parse_field(kk, vv) for kk, vv in v[0].items()}
+                primary = False
+                for name, d in fields.items():
+                    type, kwargs = d
+                    if name == Storage.default_field:
+                        raise ValueError(f'invalid field name: "{name}')
+                    if "primary_key" in kwargs:
+                        primary = True
+                if not primary:
+                    raise ValueError(f'no primary key defined for collection "{k}"')
+                collections[k] = fields
+            else:
+                type_str, kwargs = cls._parse_field(k, v)
+                fields = collections.setdefault(Storage.default_collection, {})
+                if Storage.default_field not in fields:
+                    fields[Storage.default_field] = ["str", {"primary_key": True}]
+                fields[k] = [type_str, kwargs]
+        return collections
+
+    @classmethod
+    def _check_schema(cls, schema):
+        result = {}
+        for k, v in schema.items():
+            if isinstance(v, dict):
+                v = {
+                    k: (type_to_str(i) if isinstance(i, type) else i)
+                    for k, i in v.items()
+                }
+            elif isinstance(v, list):
+                if len(v) == 1 and isinstance(v[0], dict):
+                    v = [cls._check_schema(v[0])]
+                elif (
+                    len(v) == 2
+                    and isinstance(v[0], (str, type))
+                    and isinstance(v[1], dict)
+                ):
+                    if isinstance(v[0], type):
+                        v = [type_to_str(v[0]), v[1]]
+                else:
+                    raise ValueError("invalid schema definition for field {k}: {v}")
+            if isinstance(v, type):
+                v = type_to_str(v)
+            result[k] = v
+        return result
+
+    def add_schema(self, name, version=None):
+        schema_to_collections = self.find_schema(name, version)
+        if not schema_to_collections:
+            raise ValueError(f"cannot find schema {name} with version {version}")
+        with self.session(write=True, exclusive=True) as dbs:
+            dbs._server.add_schema_collections(
+                dbs._connection_id, schema_to_collections
+            )
 
     @contextmanager
     def session(self, exclusive=False, write=False):
         token = self.server.access_rights("TODO")
         connection_id = self.server.connect(
-            token, self.get_schema(), exclusive=exclusive, write=write
+            token, exclusive=exclusive, write=write
         )
         try:
             yield StorageSession(self.server, connection_id)
@@ -20,52 +178,6 @@ class Storage:
         except Exception:
             self.server.disconnect(connection_id, rollback=True)
             raise
-
-    @classmethod
-    def update_schema(cls, result, other):
-        for k, v in other.items():
-            mine = result.get(k)
-            if mine is None:
-                if isinstance(v, dict):
-                    v = {
-                        k: (type_to_str(i) if isinstance(i, type) else i)
-                        for k, i in v.items()
-                    }
-                elif isinstance(v, list):
-                    if len(v) == 1 and isinstance(v[0], dict):
-                        inner = {}
-                        cls.update_schema(inner, v[0])
-                        v = [inner]
-                    elif (
-                        len(v) == 2
-                        and isinstance(v[0], (str, type))
-                        and isinstance(v[1], dict)
-                    ):
-                        if isinstance(v[0], type):
-                            v = [type_to_str(v[0]), v[1]]
-                    else:
-                        raise ValueError("invalid schema definition for field {k}: {v}")
-                if isinstance(v, type):
-                    v = type_to_str(v)
-                result[k] = v
-            else:
-                if isinstance(v, dict):
-                    result[k].update(v)
-                elif isinstance(v, list):
-                    if v and isinstance(v[0], dict):
-                        result[k][0].update(v)
-                else:
-                    if isinstance(v, type):
-                        v = type_to_str(v)
-                    result[k] = v
-
-    def get_schema(self):
-        schema = {}
-        for cls in reversed(self.__class__.__mro__):
-            s = getattr(cls, "schema", None)
-            if s:
-                self.update_schema(schema, s)
-        return schema
 
 
 class StorageSession:
@@ -100,3 +212,28 @@ class StorageSession:
 
     def distinct_values(self, field):
         return self._server.distinct_values(self._connection_id, self._path, field)
+
+    def add_schema(self, name, version_selection=None):
+        if self._path:
+            raise ValueError("add_schema is only allowed on database session")
+        schema_info = Storage.find_schema(name, version_selection)
+        if not schema_info:
+            raise ValueError(
+                f"cannot find schema {name} with version {version_selection}"
+            )
+        return self._server.add_schema(self._connection_id, schema_info)
+
+    # def ensure_schema(self, schema, version=None):
+    #     if self._path:
+    #         raise ValueError("ensure_schema is only allowed on database session")
+    #     return self._server.ensure_schema(self._connection_id, schema, version)
+
+    # def schema_names(self):
+    #     if self._path:
+    #         raise ValueError("schema_names is only allowed on database session")
+    #     return self._server.schema_names(self._connection_id)
+
+    # def schema(self, schema_name):
+    #     if self._path:
+    #         raise ValueError("schema is only allowed on database session")
+    #     return self._server.schema(self._connection_id, schema_name)
--- python/populse_db/storage_server.py
+++ python/populse_db/storage_server.py
@@ -3,13 +3,10 @@ from uuid import uuid4
 from populse_db.database import str_to_type
 
 from . import Database
+import populse_db.storage
 
 
-class StorageServer:
-    default_collection = "_"
-    default_field = "_"
-    default_document_id = "_"
-
+class StorageClient:
     def __init__(self, *args, **kwargs):
         self.database = Database(*args, **kwargs)
         self.connections = {}
@@ -17,20 +14,25 @@ class StorageServer:
     def access_rights(self, access_token):
         return "write"
 
-    def connect(self, access_token, schema, exclusive, write):
+    def connect(self, access_token, exclusive, write):
         connection_id = str(uuid4())
         access_rights = self.access_rights(access_token)
         if not write and access_rights in ("read", "write"):
             self.connections[connection_id] = StorageServerRead(
-                self.database, schema, exclusive
+                self.database, exclusive
             )
         elif access_rights == "write":
             self.connections[connection_id] = StorageServerWrite(
-                self.database, schema, exclusive
+                self.database, exclusive
             )
         else:
             raise PermissionError(f"database access refused")
         return connection_id
+
+    def add_schema_collections(self, connection_id, schema_to_collections):
+        return self.connections[connection_id].add_schema_collections(
+            schema_to_collections
+        )
 
     def disconnect(self, connection_id, rollback):
         self.connections[connection_id]._close(rollback)
@@ -54,107 +56,106 @@ class StorageServer:
 class StorageServerRead:
     _read_only_error = "database is read-only"
 
-    def __init__(self, database, schema, exclusive):
+    def __init__(self, database, exclusive):
         self._dbs = database.session(exclusive=exclusive)
-        self._check_schema(schema, create=False)
 
     def _close(self, rollback):
         self._dbs.close(rollback=rollback)
 
-    def _check_collection(self, collection_name, definition, create):
-        primary_key = []
-        collection_fields = {}
-        for kk, vv in definition.items():
-            if isinstance(vv, str):
-                collection_fields[kk] = (str_to_type(vv), {})
-            elif isinstance(vv, list) and len(vv) == 2:
-                if isinstance(vv[0], type):
-                    t = vv[0]
-                else:
-                    t = str_to_type(vv[0])
-                kwargs = vv[1].copy()
-                if kwargs.pop("primary_key", None):
-                    primary_key.append(kk)
-                collection_fields[kk] = (t, kwargs)
-        if not self._dbs.has_collection(collection_name):
-            if create:
-                if not primary_key:
-                    raise ValueError(
-                        f"invalid schema, collection {collection_name} is "
-                        "missing and cannot be created without primary key"
-                    )
-                self._dbs.add_collection(collection_name, primary_key)
-            else:
-                raise LookupError(
-                    f"collection {collection_name} is required in the schema but is not in the database"
-                )
-        collection = self._dbs[collection_name]
-        if primary_key and list(collection.primary_key) != primary_key:
-            raise ValueError(
-                f"primary key of collection {collection_name} is {list(collection.primary_key)} in database but is {primary_key} in schema"
-            )
-        for n, d in collection_fields.items():
-            f = collection.fields.get(n)
-            t, kwargs = d
-            if f:
-                if f["type"] != t:
-                    raise ValueError(
-                        f"type of field {collection_name}.{n} is {f['type']} in database but is {t} in schema"
-                    )
-            else:
-                collection.add_field(n, t, **kwargs)
+    # def _check_collection(self, collection_name, definition, create):
+    #     primary_key = []
+    #     collection_fields = {}
+    #     for kk, vv in definition.items():
+    #         if isinstance(vv, str):
+    #             collection_fields[kk] = (str_to_type(vv), {})
+    #         elif isinstance(vv, list) and len(vv) == 2:
+    #             if isinstance(vv[0], type):
+    #                 t = vv[0]
+    #             else:
+    #                 t = str_to_type(vv[0])
+    #             kwargs = vv[1].copy()
+    #             if kwargs.pop("primary_key", None):
+    #                 primary_key.append(kk)
+    #             collection_fields[kk] = (t, kwargs)
+    #     if not self._dbs.has_collection(collection_name):
+    #         if create:
+    #             if not primary_key:
+    #                 raise ValueError(
+    #                     f"invalid schema, collection {collection_name} is "
+    #                     "missing and cannot be created without primary key"
+    #                 )
+    #             self._dbs.add_collection(collection_name, primary_key)
+    #         else:
+    #             raise LookupError(
+    #                 f"collection {collection_name} is required in the schema but is not in the database"
+    #             )
+    #     collection = self._dbs[collection_name]
+    #     if primary_key and list(collection.primary_key) != primary_key:
+    #         raise ValueError(
+    #             f"primary key of collection {collection_name} is {list(collection.primary_key)} in database but is {primary_key} in schema"
+    #         )
+    #     for n, d in collection_fields.items():
+    #         f = collection.fields.get(n)
+    #         t, kwargs = d
+    #         if f:
+    #             if f["type"] != t:
+    #                 raise ValueError(
+    #                     f"type of field {collection_name}.{n} is {f['type']} in database but is {t} in schema"
+    #                 )
+    #         else:
+    #             collection.add_field(n, t, **kwargs)
 
-    def _check_schema(self, schema, create):
-        if not self._dbs.has_collection(StorageServer.default_collection):
-            if create:
-                self._dbs.add_collection(
-                    StorageServer.default_collection, StorageServer.default_field
-                )
-            else:
-                raise LookupError("database was not properly initialized")
-        default_collection = self._dbs[StorageServer.default_collection]
-        for k, v in schema.items():
-            if isinstance(v, dict):
-                # Create the collection first to set its primary key
-                self._check_collection(
-                    k,
-                    {StorageServer.default_field: [str, {"primary_key": True}]},
-                    create,
-                )
-                # Then call create again to add fields from schema definition
-                # and raise an error if a primary key is defined.
-                self._check_collection(k, v, create)
-                if not self._dbs[k].has_document(StorageServer.default_document_id):
-                    if create:
-                        # Create an empty singleton document
-                        self._dbs[k][StorageServer.default_document_id] = {}
-                    else:
-                        raise LookupError(
-                            f"single document is missing from collection {k}"
-                        )
-            elif isinstance(v, list) and len(v) == 1 and isinstance(v[0], dict):
-                self._check_collection(k, v[0], create)
-            elif isinstance(v, str):
-                v = str_to_type(v)
-                f = default_collection.fields.get(k)
-                if f:
-                    if f["type"] != v:
-                        raise ValueError(
-                            f"type of field {k} is {f['type']} in database but is {v} in schema"
-                        )
-                else:
-                    default_collection.add_field(k, v)
-            else:
-                raise ValueError(f"invalid schema definition for field {k}: {v}")
-        if not self._dbs[StorageServer.default_collection].has_document(
-            StorageServer.default_document_id
-        ):
-            if create:
-                self._dbs[StorageServer.default_collection][
-                    StorageServer.default_document_id
-                ] = {}
-            else:
-                raise LookupError("global document is missing from database")
+    # def _check_schema(self, schema, create):
+    #     if not self._dbs.has_collection(StorageServer.default_collection):
+    #         if create:
+    #             self._dbs.add_collection(
+    #                 StorageServer.default_collection, StorageServer.default_field
+    #             )
+    #         else:
+    #             raise LookupError("database was not properly initialized")
+    #     default_collection = self._dbs[StorageServer.default_collection]
+    #     for k, v in schema.items():
+    #         if isinstance(v, dict):
+    #             # Create the collection first to set its primary key
+    #             self._check_collection(
+    #                 k,
+    #                 {StorageServer.default_field: [str, {"primary_key": True}]},
+    #                 create,
+    #             )
+    #             # Then call create again to add fields from schema definition
+    #             # and raise an error if a primary key is defined.
+    #             self._check_collection(k, v, create)
+    #             if not self._dbs[k].has_document(StorageServer.default_document_id):
+    #                 if create:
+    #                     # Create an empty singleton document
+    #                     self._dbs[k][StorageServer.default_document_id] = {}
+    #                 else:
+    #                     raise LookupError(
+    #                         f"single document is missing from collection {k}"
+    #                     )
+    #         elif isinstance(v, list) and len(v) == 1 and isinstance(v[0], dict):
+    #             self._check_collection(k, v[0], create)
+    #         elif isinstance(v, str):
+    #             v = str_to_type(v)
+    #             f = default_collection.fields.get(k)
+    #             if f:
+    #                 if f["type"] != v:
+    #                     raise ValueError(
+    #                         f"type of field {k} is {f['type']} in database but is {v} in schema"
+    #                     )
+    #             else:
+    #                 default_collection.add_field(k, v)
+    #         else:
+    #             raise ValueError(f"invalid schema definition for field {k}: {v}")
+    #     if not self._dbs[StorageServer.default_collection].has_document(
+    #         StorageServer.default_document_id
+    #     ):
+    #         if create:
+    #             self._dbs[StorageServer.default_collection][
+    #                 StorageServer.default_document_id
+    #             ] = {}
+    #         else:
+    #             raise LookupError("global document is missing from database")
 
     def _parse_path(self, path):
         if not path:
@@ -163,9 +164,9 @@ class StorageServerRead:
             collection = self._dbs[path[0]]
             path = path[1:]
         else:
-            collection = self._dbs[StorageServer.default_collection]
-        if StorageServer.default_field in collection.primary_key:
-            document_id = StorageServer.default_document_id
+            collection = self._dbs[populse_db.storage.Storage.default_collection]
+        if populse_db.storage.Storage.default_field in collection.primary_key:
+            document_id = populse_db.storage.Storage.default_document_id
         else:
             if not path:
                 return (collection, None, None, None)
@@ -188,7 +189,7 @@ class StorageServerRead:
             return value
         elif document_id:
             document = collection[document_id]
-            if document_id == StorageServer.default_document_id:
+            if document_id == populse_db.storage.Storage.default_document_id:
                 del document[document_id]
             return document
         else:
@@ -215,9 +216,50 @@ class StorageServerRead:
 
 
 class StorageServerWrite(StorageServerRead):
-    def __init__(self, database, schema, exclusive):
+    def __init__(self, database, exclusive):
         self._dbs = database.session(exclusive=exclusive)
-        self._check_schema(schema, create=True)
+
+    def add_schema_collections(self, schema_to_collections):
+        if not self._dbs.has_collection(populse_db.storage.Storage.schema_collection):
+            self._dbs.add_collection(
+                populse_db.storage.Storage.schema_collection, "name"
+            )
+            schema_collection = self._dbs[populse_db.storage.Storage.schema_collection]
+            schema_collection.add_field("version", str)
+            schema_collection[schema_to_collections["name"]] = {
+                "version": schema_to_collections["version"]
+            }
+        collections = schema_to_collections["collections"]
+        for collection_name, fields_definition in collections.items():
+            if not self._dbs.has_collection(collection_name):
+                primary_keys = {}
+                for field_name, field_def in fields_definition.items():
+                    type_str, kwargs = field_def
+                    if kwargs.get("primary_key", False):
+                        primary_keys[field_name] = str_to_type(type_str)
+                self._dbs.add_collection(collection_name, primary_keys)
+                if populse_db.storage.Storage.default_field in primary_keys:
+                    self._dbs[collection_name][
+                        populse_db.storage.Storage.default_document_id
+                    ] = {}
+
+            collection = self._dbs[collection_name]
+            for field_name, field_def in fields_definition.items():
+                type_str, kwargs = field_def
+                field = collection.fields.get(field_name)
+                if field:
+                    type_python = str_to_type(type_str)
+                    if field["type"] != type_python:
+                        raise ValueError(
+                            f"database has a {collection_name}.{field_name} field "
+                            f"with type {field['type']} but schema requires type {type_python}"
+                        )
+                    if field["primary_key"] != kwargs.get("primary_key", False):
+                        raise ValueError(
+                            f"primary key difference between database and schema for {collection_name}.{field_name}"
+                        )
+                else:
+                    collection.add_field(field_name, str_to_type(type_str), **kwargs)
 
     def set(self, path, value):
         collection, document_id, field, path = self._parse_path(path)
