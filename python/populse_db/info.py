b697f166f6574b19c2cdf345b1b05908dafbf138
econdami <econdami@yahoo.fr>
2023-12-20 12:33:58 +0100

    bump to version 2.5.2

--- python/populse_db/info.py
+++ python/populse_db/info.py
@@ -19,7 +19,7 @@ import sys
 # populse_db current version
 version_major = 2
 version_minor = 5
-version_micro = 1
+version_micro = 2
 version_extra = ""
 
 # Expected by setup.py: string of form "X.Y.Z"
